80a25d6660f96585120dce7ffde5aa78dfd17699
Hope <hope_chambers@comcast.com>
2022-09-16 18:00:30 -0400

    All of two completed and all tests passed for two

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -12,8 +14,17 @@ import java.util.Optional;
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
+    @Autowired
+    private EmployerRepository employerRepository;
 
+    @GetMapping("")
+    public String index(Model model) {
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("title", "All Employers");
+        return "employers/index";
 
+    }
+//created the find all method to search through and find all the employers to search through//
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
         model.addAttribute(new Employer());
@@ -27,20 +38,26 @@ public class EmployerController {
         if (errors.hasErrors()) {
             return "employers/add";
         }
-
+        employerRepository.save(newEmployer);
+//this is saving the new employer information once it is added back to the data base//
         return "redirect:";
     }
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional  optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
+            model.addAttribute("title", "Edit Employer  " + employer.getName() + " (id=" + employer.getId() + ")");
             return "employers/view";
         } else {
             return "redirect:../";
         }
     }
+
+
+
 }
+
